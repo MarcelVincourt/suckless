d8312bf39efd3f6feaec70cade8403c725c69a5a
marcel <marcel@thinkpad-t480.thinkpad>
2025-04-25 17:43:31 +0200

    adding bar code and extra fonts

---
+++ dwm-bar/bar-functions/dwm_date.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# A dwm_bar function that shows the current date and time
+# GNU GPLv3
+
+# Date is formatted like like this: "[Mon 01-01-00 00:00:00]"
+dwm_date () {
+    printf "%s" "$SEP1"
+    if [ "$IDENTIFIER" = "unicode" ]; then
+        #printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+        printf "📆 %s" "$(date "+%F %R")"
+    else
+        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+    fi
+    printf "%s\n" "$SEP2"
+}
+
+dwm_date
